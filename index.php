a59d3f070a8bcf7d73027ba4b3435beed1b2ddb4
jennebumke <jennebumke@live.nl>
2016-09-01 12:13:01 +0200

    functional style complete and hidden tag added

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 ?>
 <html>
 	<head>
-		<script   src="https://code.jquery.com/jquery-3.1.0.min.js"   integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s="   crossorigin="anonymous"></script>
+		<script src="https://code.jquery.com/jquery-3.1.0.min.js"   integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s="   crossorigin="anonymous"></script>
 		<script src="js/ministeckBlock.js"></script>
 		<script src="js/ministeckPiece.js"></script>
 		<script src="js/ministeckColor.js"></script>
@@ -13,12 +13,13 @@
 		<link href="css/style.css" rel="stylesheet" type="text/css">
 	</head>
 	<body>
+		<input type="hidden" name="inputDoc" value="">
 		<script>
 		//initialize
 		
 		</script>
 		<form id="input" action="" method="POST">
-			<input type="file" name="Upload">
+			<input type="file" name="Upload" id="input">
 			<div id="load">
 				<img src="load.gif">
 				<br>
