421b8e500bc0069a8c190e050c51a972f04c54f7
jennebumke <jennebumke@live.nl>
2016-09-01 10:07:20 +0200

    Index edit scripts

--- index.php
+++ index.php
@@ -3,7 +3,10 @@
 ?>
 <html>
 	<head>
-		<script src="js/Ministeck.js"></script>
+		<script src="js/miniSteckGenerator.js"></script>
+		<script src="js/miniSteckBlock.js"></script>
+		<script src="js/miniSteckPiece.js"></script>
+		<title>MiniSteck Online</title>
 	</head>
 	<body>
 		
