793a77edfa1d0a90e40472487ea7ff36599111fc
jennebumke <jennebumke@live.nl>
2016-09-01 10:36:23 +0200

    Most HTML + more scripts

--- index.php
+++ index.php
@@ -3,12 +3,21 @@
 ?>
 <html>
 	<head>
-		<script src="js/miniSteckGenerator.js"></script>
-		<script src="js/miniSteckBlock.js"></script>
-		<script src="js/miniSteckPiece.js"></script>
-		<title>MiniSteck Online</title>
+		<script src="js/ministeckGenerator.js"></script>
+		<script src="js/ministeckBlock.js"></script>
+		<script src="js/ministeckPiece.js"></script>
+		<script src="js/ministeckColor.js"></script>
+		<script src="js/ministeckSymColorPair.js"></script>
+		<title>Ministeck Online</title>
+		<link src="css/style.css" rel="stylesheet" type="text/css">
 	</head>
 	<body>
-		
+		<form id="input" action="" method="POST">
+			<button type="file">Upload</button>
+			<div id="load">
+				<img src="load.gif">
+				<span id="process">0%</span>
+			</div>
+		</form>
 	</body>
 </html>
